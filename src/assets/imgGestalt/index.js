34f880621d999171f70208228abe3fb22cbfe149
marimar <andreo.olivella@gmail.com>
2025-10-26 15:14:13 +0100

    .

---
+++ src/assets/imgGestalt/index.js
@@ -0,0 +1,6 @@
+// Exporta cada imagen individualmente
+export { default as imagen1 } from 'cierreImg1.png';
+export { default as imagen2 } from 'cierreimg2.png';
+export { default as imagen3 } from 'cierreimg3.png';
+export { default as imagen4 } from 'cierreImg4.png';
+// Agrega todas tus imágenes aquí
